7d840757966723e2f050b13044241520c2d6980b
Daniel Tang <danielzgtg.opensource@gmail.com>
2025-08-24 22:30:43 -0400

    Install to /etc/modules

--- dkms.sh
+++ dkms.sh
@@ -6,6 +6,7 @@ DIR="$(readlink -f "$(dirname "$0")")"
 MODULE="$(grep 'PACKAGE_NAME' "$DIR/dkms.conf" | cut -d'=' -f2 | grep -Eo '[^"]+')"
 
 uninstall() {
+    sed -i '/surface-acpi-tad-rtc/d' /etc/modules
     for mod in $(dkms status | grep "$MODULE" | cut -d',' -f1); do
         dkms remove "$mod" --force --all
     done
@@ -15,6 +16,7 @@ install() {
     uninstall
     dkms add "$DIR"
     dkms autoinstall --force
+    echo surface-acpi-tad-rtc >> /etc/modules
 }
 
 case "$1" in
